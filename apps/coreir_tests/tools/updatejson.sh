93abdae41bbf652919bf96ef048cb42d6b3da69f
Jeff Setter <setter@stanford.edu>
2018-02-21 17:09:41 -0800

    added coreir tests which tests all features from Halide to CoreIR

---
+++ apps/coreir_tests/tools/updatejson.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HALIDE_PATH="Halide_CoreIR"
+MAPPER_PATH="CGRAMapper"
+
+if [ ! -d ${HALIDE_PATH} ]; then
+    echo "Specified Halide path does not exist."
+    exit 1
+fi
+if [ ! -d ${MAPPER_PATH} ]; then
+    echo "Specified mapper path does not exist."
+    exit 1
+fi
+
+#make -C ${HALIDE_PATH}/apps/coreir_examples cleanall 
+make -j8 -C ${HALIDE_PATH}/apps/coreir_examples test_all && echo "Completed creating json apps"
+
+APPS=${HALIDE_PATH}/apps/coreir_examples/*
+echo $APPS
+
+for app in $APPS
+do
+if [[ -f "${app}/design_prepass.json" ]]; then
+    appname=`basename $app`
+    echo "copied json from $appname"
+    cp ${app}/design_prepass.json ${MAPPER_PATH}/examples/${appname}.json
+else
+    appname=`basename $app`
+    echo "$appname does not have a json"
+fi
+done
---
+++ apps/coreir_tests/uminmax/pipeline.cpp
@@ -0,0 +1,106 @@
+#include "Halide.h"
+#include <stdio.h>
+
+using namespace Halide;
+
+Var x("x"), y("y");
+Var xo("xo"), yo("yo"), xi("xi"), yi("yi");
+
+class MyPipeline {
+public:
+    ImageParam in;
+    Func hw_input;
+    Func umin, umax, uclamp;
+    Func output, hw_output;
+    std::vector<Argument> args;
+
+    MyPipeline()
+      : in(UInt(8), 2),
+	output("output"), hw_output("hw_output")
+    {
+        // Pointwise operations
+        hw_input(x,y) = cast<uint8_t>( in(x,y) );
+
+        umin(x,y) = min( hw_input(x,y) , 128 );
+				umax(x,y) = max( hw_input(x,y) , 64 );
+				uclamp(x,y) = clamp( hw_input(x,y), 1, 150 );
+
+        hw_output(x, y) = umin(x,y) + umax(x,y) - uclamp(x,y);
+        output(x, y) = hw_output(x, y);
+
+        // Arguments
+        args.push_back(in);
+    }
+
+    void compile_cpu() {
+        std::cout << "\ncompiling cpu code..." << std::endl;
+        //kernel.compute_root();
+
+        output.tile(x, y, xo, yo, xi, yi, 64, 64)
+            .vectorize(xi, 8)
+            .fuse(xo, yo, xo).parallel(xo);
+
+        output.compile_to_lowered_stmt("pipeline_native.ir.html", args, HTML);
+        output.compile_to_header("pipeline_native.h", args, "pipeline_native");
+        output.compile_to_object("pipeline_native.o", args, "pipeline_native");
+    }
+
+    void compile_gpu() {
+        std::cout << "\ncompiling gpu code..." << std::endl;
+
+        output.gpu_tile(x, y, 32, 16);
+
+        //output.print_loop_nest();
+
+        Target target = get_target_from_environment();
+        target.set_feature(Target::CUDA);
+        output.compile_to_lowered_stmt("pipeline_cuda.ir.html", args, HTML, target);
+        output.compile_to_header("pipeline_cuda.h", args, "pipeline_cuda", target);
+        output.compile_to_object("pipeline_cuda.o", args, "pipeline_cuda", target);
+    }
+
+    void compile_hls() {
+        std::cout << "\ncompiling HLS code..." << std::endl;
+	hw_input.compute_root();
+        hw_output.compute_root();
+
+	output.tile(x, y, xo, yo, xi, yi, 64, 64);
+        hw_output.accelerate({hw_input}, xi, xo);
+
+        //output.print_loop_nest();
+        // Create the target for HLS simulation
+        Target hls_target = get_target_from_environment();
+        hls_target.set_feature(Target::CPlusPlusMangling);
+        output.compile_to_lowered_stmt("pipeline_hls.ir.html", args, HTML, hls_target);
+        output.compile_to_hls("pipeline_hls.cpp", args, "pipeline_hls", hls_target);
+        output.compile_to_header("pipeline_hls.h", args, "pipeline_hls", hls_target);
+    }
+
+  void compile_coreir() {
+        std::cout << "\ncompiling CoreIR code..." << std::endl;
+	hw_input.compute_root();
+        hw_output.compute_root();
+
+	output.tile(x, y, xo, yo, xi, yi, 64, 64);
+	hw_output.tile(x, y, xo, yo, xi, yi, 64, 64);
+	hw_output.accelerate({hw_input}, xi, xo);
+
+        Target coreir_target = get_target_from_environment();
+        coreir_target.set_feature(Target::CPlusPlusMangling);
+        output.compile_to_lowered_stmt("pipeline_coreir.ir.html", args, HTML, coreir_target);
+        output.compile_to_coreir("pipeline_coreir.cpp", args, "pipeline_coreir", coreir_target);
+    }
+};
+
+int main(int argc, char **argv) {
+    MyPipeline p1;
+    p1.compile_cpu();
+
+    MyPipeline p2;
+    p2.compile_hls();
+
+    MyPipeline p3;
+    p3.compile_coreir();
+
+    return 0;
+}
